b5203b2ed3f0ef15c6b51c641e28b3f8c9228aa6
tanu-3104 <tanusreesarder1@gmail.com>
2025-08-07 14:54:01 +0530

    adding pyhtong codes to my repo

---
+++ Python-Coding/hello-world.py
@@ -0,0 +1,172 @@
+
+#a = int(input("Enter 1st no :"))
+#b = int(input("Enter 2nd no :"))
+#print (a>=b)
+
+#print ("Hello"+ " " +"world")
+
+#str= len("Tanusree_sardar")
+#print(str)
+
+#str2="Apna_College"
+#print(str2[2:7])
+#print(str2.endswith("ege"))
+#print(str2.capitalize())
+#print(str2.replace("Apna","Mera"))
+
+##if (a>=90):
+  #  print("Grade A")
+#elif(a<90 and a>=80):
+ #   print("Grade B")
+#elif(a<80 and a>=70):
+ #   print("Grade C")
+#elif(a<70) :
+ #   print("Grade D")
+
+#age = 34
+
+#if (age>=18):
+ #   if(age>=80):
+  #      print("Cannot drive"):
+   # else("can drive")
+
+#a =int (input("enter the no"))
+
+#if(a%2==0):
+    #print("Even")
+#else:
+   # print("Odd")
+
+"""a=int(input("Enter 1st no:"))
+b=int(input("Enter 2nd no:"))
+C=int(input("Enter 3rd no:"))
+
+if (a>=b and a>=C):
+    print("First no is greatest")
+elif(b>=C):
+    print("second is largest")
+else:
+    print("third is largest") """
+
+"""marks[0]="90"    
+print(marks)
+print(marks[1:len(marks)])"""
+
+"""marks=[67,87,33,95,76]
+print(marks.sort())
+print(marks.append(90))
+print(marks)"""
+"""movies=[]
+mov1= input("enter 1st mov:")
+mov2=input("enter 2nd mov:")
+mov3=input("enter 3rd mov:")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)"""
+
+
+
+"""movies=[]
+mov=(movies.append (input("enter 1st mov:")))
+mov=(movies.append (input("enter 2nd mov:")))
+mov=(movies.append (input("enter 3rd mov:")))
+
+print(movies)"""
+
+"""tup=("C","D","A","A","B","B","B","A")
+print(tup.count("A"))"""
+
+"""list1=[1,2,1]
+list2=[1,2,3]
+
+Copy_list1 = list1.copy()
+Copy_list1.reverse
+
+if(Copy_list1==list1):
+    print("Pelindrome")
+else:
+    print("non pelindrome") """
+
+"""dict = { "name":"tanusree sardar","age":27,"sub":["political science","English","Bengali"],"Cgpa":8.9
+        } 
+dict["name"] = "trinath sardar"
+print(dict)
+
+null_dict={}
+null_dict["name"]="tanusree sardar"
+print(null_dict)
+
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+print(dict.get("sub"))"""
+
+
+"""collection= {1,2,2,2,4,3,4}
+print(collection)
+print(type(collection))
+collection2=set()#emptyset
+collection2.add(1)
+collection2.add(4)
+collection2.add(5)
+print(collection2)"""
+
+
+
+"""i=1
+while i<=100:
+    print(i)
+    i+=1"""
+
+
+"""i=100
+while i>=1:
+    print(i)
+    i-=1"""
+"""n=input("enter the value")
+x: int= int(n);
+i=1
+while i<=10:
+    print(n*i)
+    i+=1"""
+
+"""nums=[1,4,9,16,25,36,49,64,81,100]
+
+index=0
+while index<=len(nums)-1:
+     print(nums[index])
+     index +=1"""
+
+   
+"""nums=(1,4,9,16,25,36,49,64,81,100,64)
+x=64
+i=0
+while i<=len(nums)-1:
+    if(nums[i]==x):
+        print("no found in index")"""
+    #i+=1
+
+i=0
+while i<=10:
+    if(i%2==0):
+        i+=1
+        continue
+    print(i)
+    i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
